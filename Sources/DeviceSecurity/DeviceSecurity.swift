ed02e922a3924db96f408bcaca0ce1c247e4a07a
Christopher Nassar <christopher.nassar@totersapp.com>
2025-07-14 16:07:24 +0300

    Latest updates

--- Sources/DeviceSecurity/DeviceSecurity.swift
+++ Sources/DeviceSecurity/DeviceSecurity.swift
@@ -5,10 +5,20 @@
 //  Created by Christopher Nassar on 14/07/2025.
 //
 
+import Foundation
+
 public struct DeviceSecurity {
-    /// Public general API to check for any secruity breach
-    static public func isSecure() -> Bool {
-        return !RootChecker().isJailbroken() && !isRunningOnSimulator && !isDebuggerAttached
+    /// Entry point for checking device security.
+    ///
+    /// - Parameter canOpenUrlChecker: Optional closure to determine whether specific URL schemes (like `cydia://`) are available.
+    ///   You should pass `UIApplication.shared.canOpenURL(_)` from the main app context.
+    ///
+    /// - Returns: `true` if the device appears secure; otherwise, `false`.
+    public static func isSecure(canOpenUrlChecker: ((URL) -> Bool)? = nil) -> Bool {
+        let rootChecker = RootChecker(canOpenUrlChecker: canOpenUrlChecker)
+        return !rootChecker.isJailbroken()
+            && !isRunningOnSimulator
+            && !isDebuggerAttached
     }
 }
 
